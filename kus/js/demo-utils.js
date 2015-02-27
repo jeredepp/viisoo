22a4f4c4bd650b9edc426a95840deb88f0da7d17
Markus Koller <markus.koller@starfrosch.ch>
2015-02-27 15:58:01 +0100

    reduce

--- kus/js/demo-utils.js
+++ kus/js/demo-utils.js
@@ -63,57 +63,6 @@ var fluid = fluid || require("infusion"),
         return that;
     };
     
-    demo.fileSelectorView = function(synth, options) {
-        var that = {
-            input: document.querySelector(options.selectors.input),
-            button: document.querySelector(options.selectors.button),
-            fileName: document.querySelector(options.selectors.fileName)
-        };
-        
-        that.input.addEventListener("change", function () {
-            if (that.fileName) {
-                that.fileName.innerHTML = that.input.files[0].name;
-            }
-            
-            var players = fluid.makeArray(options.playerId);
-            fluid.each(players, function (id) {
-                synth.input(id).onInputChanged("buffer");
-            });
-        });
-		
-        // On Firefox, bind a click event to the browse button, which delegates to the hidden (ugly) file input element.
-        if (window.navigator.userAgent.indexOf("Firefox") !== -1) {
-            that.button.addEventListener("click", function (e) {
-                that.input.click();
-                e.preventDefault();
-            }, false);
-        } else {
-            // On Chrome, show the ugly file input element.
-            that.input.style.display = "inline";
-            that.button.style.display = "none";
-            if (that.fileName) {
-                that.fileName.style.display = "none";
-            }
-        }
-    };
-    
-    demo.dataUrlSelectorView = function (synth, options) {
-        var that = {
-            field: document.querySelector(options.selectors.field)
-        };
-        
-        that.field.addEventListener("change", function () {
-            that.dataUrl = that.field.value;
-            
-            var players = fluid.makeArray(options.playerId);
-            fluid.each(players, function (id) {
-                var player = synth.input(id),
-                    bufDef = player.input("buffer");
-            
-                bufDef.url = that.dataUrl;
-                player.onInputChanged("buffer");
-            });
-        });
-    };
+
     
 })();
